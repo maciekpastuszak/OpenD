4d83ef97c74758b9faa76e6601db5affdd0ecf6c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-03 09:59:34 +0100

    display minted image

--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -8,6 +8,7 @@ function Item(props) {
 
   const [name, setName] = useState();
   const [owner, setOwner] = useState();
+  const [image, setImage] = useState();
 
   const id = Principal.fromText(props.id);
 
@@ -23,9 +24,13 @@ function Item(props) {
 
     const name = await NFTActor.getName();
     const owner = await NFTActor.getOwner();
+    const imageData = await NFTActor.getAsset();
+    const imageContent = new Uint8Array(imageData);
+    const image = URL.createObjectURL(new Blob([imageContent.buffer], {type: "image/png"}))
+
     setName(name);
     setOwner(owner.toText());
-
+    setImage(image);
 
   }
 
@@ -38,7 +43,7 @@ function Item(props) {
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
         <img
           className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-          src={logo}
+          src={image}
         />
         <div className="disCardContent-root">
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
