f8e4a1b1a9cd45d2fae53a85f19dda3629a90667
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-05 22:36:54 +0100

    add forceRefresher to Header component

--- src/opend_assets/src/components/Header.jsx
+++ src/opend_assets/src/components/Header.jsx
@@ -22,7 +22,7 @@ function Header() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter forceRefresh={true}>
     <div className="app-root-1">
       <header className="Paper-root AppBar-root AppBar-positionStatic AppBar-colorPrimary Paper-elevation4">
         <div className="Toolbar-root Toolbar-regular header-appBar-13 Toolbar-gutters">
