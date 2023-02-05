c3bdd2302a46ae2a4978bbe34b37f831c8567c08
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-05 21:42:53 +0100

    add react router

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -2,21 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import homeImage from "../../assets/home-img.png";
 import Item from "./Item";
 import Minter from "./Minter";
 
 function App() {
 
-const NFTID = "rrkah-fqaaa-aaaaa-aaaaq-cai";
-
   return (
     <div className="App">
       <Header />
-      <Minter />
-      {/* <img className="bottom-space" src={homeImage} /> */}
-      {/* <Item id={NFTID}/>
-      <Footer /> */}
+      {/* <Minter /> */}
+      {/* <Item id={NFTID}/>*/}
+      <Footer />
     </div>
   );
 }
--- src/opend_assets/src/components/Header.jsx
+++ src/opend_assets/src/components/Header.jsx
@@ -1,30 +1,59 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import  { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import homeImage from "../../assets/home-img.png";
+import Minter from "./Minter";
+import Gallery from "./Gallery";
 
 function Header() {
   return (
+    <BrowserRouter>
     <div className="app-root-1">
       <header className="Paper-root AppBar-root AppBar-positionStatic AppBar-colorPrimary Paper-elevation4">
         <div className="Toolbar-root Toolbar-regular header-appBar-13 Toolbar-gutters">
           <div className="header-left-4"></div>
           <img className="header-logo-11" src={logo} />
           <div className="header-vertical-9"></div>
-          <h5 className="Typography-root header-logo-text">OpenD</h5>
+          <Link to="/">
+            <h5 className="Typography-root header-logo-text">OpenD</h5>
+          </Link>
           <div className="header-empty-6"></div>
           <div className="header-space-8"></div>
           <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
+            <Link to="/discover">
             Discover
+            </Link>
           </button>
           <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
+          <Link to="/minter">
             Minter
+            </Link>
           </button>
           <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
+          <Link to="/collection">
             My NFTs
+            </Link>
           </button>
         </div>
       </header>
     </div>
+    <Switch>
+      <Route path="/">
+      <img className="bottom-space" src={homeImage} />
+      </Route>
+      <Route path="/discover">
+        <h1>Discover</h1>
+      </Route>
+      <Route path="/minter">
+        <Minter />
+      </Route>
+      <Route path="/collection">
+        <Gallery />
+      </Route>
+    </Switch>
+    </BrowserRouter>
   );
 }
+
 
 export default Header;
