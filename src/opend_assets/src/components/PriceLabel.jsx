c766944022e8770228049b78c891e340ac546661
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-07 22:38:19 +0100

    add price label component

---
+++ src/opend_assets/src/components/PriceLabel.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function PriceLabel(props) {
+return (
+<div className="disButtonBase-root disChip-root makeStyles-price-23 disChip-outlined">
+          <span className="disChip-label">23 DANG</span>
+        </div>
+)};
+
+export default PriceLabel;
