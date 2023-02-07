28d1291a2add2711b1e1edaf65315cc363c199e0
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-07 09:24:35 +0100

    add sell status to my mints view

--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -15,6 +15,7 @@ function Item(props) {
   const [priceInput, setPriceInput] = useState();
   const [loaderHidden, setLoaderHidden] = useState(true);
   const [blur, setBlur] = useState();
+  const [sellStatus, setSellStatus] = useState();
 
   const id = props.id;
 
@@ -44,6 +45,7 @@ function Item(props) {
     if (nftIsListed) {
       setOwner("OpenD");
       setBlur({filter: "blur(4px)"});
+      setSellStatus("Listed");
     } else [
       setButton(<Button handleClick={handleSell} text={"Sell"}/>)
     ];
@@ -83,6 +85,7 @@ function Item(props) {
         setButton();
         setPriceInput();
         setOwner("OpenD");
+        setSellStatus("Listed");
       }
     }
   }
@@ -103,7 +106,8 @@ function Item(props) {
       </div>
         <div className="disCardContent-root">
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            {name}<span className="purple-text"></span>
+            {name}
+            <span className="purple-text"> {sellStatus}</span>
           </h2>
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
             Owner: {owner}
